67403a234035ec58645cea9c5ffbd980bab0023f
Cris M <cristian.montenegro.p@gmail.com>
2020-05-28 18:32:27 -0700

    fixing details

--- CMS.js
+++ CMS.js
@@ -444,3 +444,180 @@ removeRole = async () => {
 
 
 }
+
+// updateEmployeeManager = async () => {
+
+//     var search = [];
+//     await inquirer.prompt([{
+//         name: "first_name",
+//         type: "input",
+//         message: "What is the employee's first name?"
+//     }, {
+//         name: "last_name",
+//         type: "input",
+//         message: "What is the employee's last name?"
+//     }]).then(async ({
+//         first_name,
+//         last_name
+//     }) => {
+//         var sql = `SELECT employee.employee_id, employee.first_name, employee.last_name, role.title, department.department_name FROM ((role INNER JOIN employee ON employee.role_id = role.role_id) INNER JOIN department ON role.department_id = department.department_id) WHERE (first_name, last_name) = ("${first_name}", "${last_name}");`
+//         var res = await new CRUD(null, null, sql, true).read();
+//         // console.log(res);
+//         search = res.map(({
+//             employee_id,
+//             first_name,
+//             last_name,
+//             title,
+//             department_name
+//         }) => ({
+//             name: `First name: ${first_name}  Last name: ${last_name}  Title: ${title}  Department: ${department_name}  Employee ID: ${employee_id}`,
+//             value: employee_id
+//         }));
+//         search.push({
+//             name: "Go back",
+//             value: -1
+//         });
+
+//         inquirer.prompt([{
+//             name: "employee_id",
+//             type: "list",
+//             message: "Which of this Employee's manager is the one you'd wish to edit",
+//             choices: search
+
+//         }]).then(async function ({
+//             employee_id
+//         }) {
+
+//             // var table = "employee";
+//             // var columns = "employee_id";
+//             // var data = employee_id;
+//             if (employee_id === -1) {
+//                 return runInquirer();
+//             };
+
+//         })
+//     })
+
+// console.log(employee_id)
+
+// var sql = `SELECT employee.employee_id, employee.first_name, employee.last_name, role.title, department.department_name FROM ((role INNER JOIN employee ON employee.role_id = role.role_id) INNER JOIN department ON role.department_id = department.department_id) WHERE employee.manager_id IS NULL;`
+// var managers = await new CRUD(null, null, sql, true).read();
+
+// var managersChoices = managers.map(({
+//     employee_id,
+//     first_name,
+//     last_name,
+//     title,
+//     department_name
+// }) => ({
+//     name: `Name: ${first_name} ${last_name}  Title: ${title}  Department: ${department_name}`,
+//     value: employee_id
+// }));
+
+
+
+
+
+
+//     // var res = await new CRUD(table, columns, data).delete();
+//     if (res.serverStatus === 2) {
+//         var message = null;
+//         search.forEach(element => {
+//             if (element.value === data) {
+//                 message = element.name;
+//             }
+//         });
+//         console.table("Employee " + message + " Was removed succesfully");
+//     }
+//     setTimeout((function () {
+//         runInquirer();
+//     }), 1000);
+
+
+
+
+
+
+
+
+
+
+
+//     var sql = `SELECT employee.employee_id, employee.first_name, employee.last_name, role.title, department.department_name FROM ((role INNER JOIN employee ON employee.role_id = role.role_id) INNER JOIN department ON role.department_id = department.department_id) WHERE employee.manager_id IS NULL;`
+//     var managers = await new CRUD(null, null, sql, true).read();
+
+//     var managersChoices = managers.map(({
+//         employee_id,
+//         first_name,
+//         last_name,
+//         title,
+//         department_name
+//     }) => ({
+//         name: `Name: ${first_name} ${last_name}  Title: ${title}  Department: ${department_name}`,
+//         value: employee_id
+//     }));
+
+//     var roleRes = await new CRUD("role", "*", null).read();
+//     var roleChoices = roleRes.map(({
+//         role_id,
+//         title
+//     }) => ({
+//         name: title,
+//         value: role_id
+//     }));
+
+//     inquirer.prompt([{
+//         name: "first_name",
+//         type: "input",
+//         message: "What is the Employee's First Name?"
+//     }, {
+//         name: "last_name",
+//         type: "input",
+//         message: "What is the Employee's Last Name?"
+//     }, {
+//         name: "role_id",
+//         type: "list",
+//         message: "Which of this roles does the employee have?",
+//         choices: roleChoices
+//     }, {
+//         name: "is_manager",
+//         type: "confirm",
+//         message: "Is this Employee a Manager?"
+
+//     }]).then(async function ({
+//         first_name,
+//         last_name,
+//         role_id,
+//         is_manager
+//     }) {
+
+//         var table = "employee";
+//         var columns = ["first_name", "last_name", "role_id"];
+//         var data = [first_name, last_name, role_id];
+
+//         if (is_manager === false) {
+//             await inquirer.prompt({
+//                 name: "who_is_manager",
+//                 type: "list",
+//                 message: "Who is your manager?",
+//                 choices: managersChoices
+//             }).then(async function ({
+//                 who_is_manager
+//             }) {
+//                 columns = ["first_name", "last_name", "role_id", "manager_id"];
+//                 data = [first_name, last_name, role_id, who_is_manager];
+//             })
+//         };
+
+//         var createEmployee = await new CRUD(table, columns, data).create();
+
+//         if (createEmployee.serverStatus === 2) {
+//             console.table(`Employee ${first_name} ${last_name} Was updated with the ID: ${createEmployee.insertId}`);
+//         }
+
+//         setTimeout((function () {
+//             runInquirer();
+//         }), 1000);
+//     })
+
+// };
